d6fbcf673899c77b8ef551e563d930d7722894a8
jancyranij <41039298+jancyranij@users.noreply.github.com>
2018-09-13 08:12:19 -0700

    Create largeno.py

---
+++ largeno.py
@@ -0,0 +1,11 @@
+print("check the largest number")
+a=input("enter the number")
+b=input("enter the number")
+c=input("enter the number")
+if(a>b or a<b):
+    print(a)
+elif(c>a or c<a):
+    print(c)
+else:
+    print(b)
+        
