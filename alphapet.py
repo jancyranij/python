93440790b54d3c5e43dd073f848c05016df6e557
jancyranij <41039298+jancyranij@users.noreply.github.com>
2018-09-17 07:50:39 -0700

    Create alphapet.py

---
+++ alphapet.py
@@ -0,0 +1,8 @@
+x=input("z=")
+z=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for y in z:
+    if(y==x):
+        print("alphabet")
+        break
+else:
+    print("not")
