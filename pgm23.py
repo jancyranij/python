f147383cc92e974a58f66a6a2f7f93d821fad3d2
jancyranij <41039298+jancyranij@users.noreply.github.com>
2018-10-01 14:59:36 +0530

    Create pgm23.py

---
+++ pgm23.py
@@ -0,0 +1,10 @@
+print("print minimum element")
+from array import*
+n=int(input("N="))
+for y in range(1,n-1):
+   z=array('i',[y])
+m=z[0]
+for x in z:
+   if(x<=z[0]):
+       m=x
+print("mini",m)
