45904dc8d848ec613de306b27f2c4a2e84dc1d74
jancyranij <41039298+jancyranij@users.noreply.github.com>
2018-12-29 20:28:24 +0530

    Create p52.py

---
+++ p52.py
@@ -0,0 +1,8 @@
+def main():
+        try:
+                k=['zero','one','two','three','four','five','six','seven','eight','nine','ten']
+                x=int(input())
+                print(k[x])
+        except:
+                print('invalid')
+main()
