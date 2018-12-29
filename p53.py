c64e2aa086bd3d188cae6b506519c4760aa41002
jancyranij <41039298+jancyranij@users.noreply.github.com>
2018-12-29 20:32:14 +0530

    Create p53.py

---
+++ p53.py
@@ -0,0 +1,10 @@
+try:
+        x=int(input())
+        s=0
+        while(x!=0):
+                r=x%10
+                s+=r
+                x=x//10
+        print(s)
+except:
+        print('invalid')
